eabd666ee4bd88cb664d24810532c7983a3714ba
Box-Cat <pjh211pjh@gmail.com>
2024-06-26 23:55:02 +0900

    디버깅 세팅 - launch.json

--- src/components/VisualNovelEngine .jsx
+++ src/components/VisualNovelEngine .jsx
@@ -7,6 +7,7 @@ const VisualNovelEngine = () => {
 
   useEffect(() => {
     setData(jsonData);
+    console.log('시발')
   }, []);
 
   if (!data) return <div>Loading...</div>;
@@ -30,7 +31,7 @@ const VisualNovelEngine = () => {
         const character = Object.keys(s)[0];
         return {
           character,
-          sprite: data.Characters[character][s[character]]
+          sprite: data.Characters[character][s[character]] //어두운 그림은 "Happy"말고 추가
         };
       })
     : [{
@@ -39,19 +40,17 @@ const VisualNovelEngine = () => {
       }];
 
   return (
-    <div style={{ backgroundImage: `url(${data.Scene1.Background})`, padding: '20px' }}>
-      <div style={{ marginBottom: '20px' }}>
+    <div id="background" style={{ backgroundImage: `url(${data.Scene1.Background})`, padding: '20px' }}>
+      <div id = 'mainbox' style={{ marginBottom: '20px' }}>
         {sprites.map(({ character, sprite }, index) => (
           <div key={index} style={{ display: 'inline-block', marginRight: '10px' }}>
             {sprite && <img src={sprite} alt={character} style={{ width: '150px', height: '150px' }} />}
-            {/* <div>
-              <h2>{character}</h2>
-            </div> */}
+          
           </div>
         ))}
         <div>
-          <h2>{page.Character}</h2>
-          <p>{page.PageText}</p>
+          <h2 id='spritebox'>{page.Character}</h2>
+          <p id='textbox'>{page.PageText}</p>
         </div>
       </div>
       {page.Options ? (
