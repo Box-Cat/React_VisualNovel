c34ecf7c9216334eb9442f24bd23f388fa6060bd
Box-Cat <pjh211pjh@gmail.com>
2024-06-27 00:14:21 +0900

    HTML과 CSS 간단한 정리

--- src/components/VisualNovelEngine .jsx
+++ src/components/VisualNovelEngine .jsx
@@ -7,7 +7,6 @@ const VisualNovelEngine = () => {
 
   useEffect(() => {
     setData(jsonData);
-    console.log('시발')
   }, []);
 
   if (!data) return <div>Loading...</div>;
@@ -31,39 +30,59 @@ const VisualNovelEngine = () => {
         const character = Object.keys(s)[0];
         return {
           character,
-          sprite: data.Characters[character][s[character]] //어두운 그림은 "Happy"말고 추가
+          sprite: data.Characters[character][s[character]], //어두운 그림은 "Happy"말고 추가
         };
       })
-    : [{
-        character: page.Character,
-        sprite: data.Characters[page.Character][page.Sprite]
-      }];
+    : [
+        {
+          character: page.Character,
+          sprite: data.Characters[page.Character][page.Sprite],
+        },
+      ];
 
   return (
-    <div id="background" style={{ backgroundImage: `url(${data.Scene1.Background})`, padding: '20px' }}>
-      <div id = 'mainbox' style={{ marginBottom: '20px' }}>
+    <div
+      id="background"
+      style={{
+        backgroundImage: `url(${data.Scene1.Background})`,
+        padding: '20px',
+      }}
+    >
+      <div id="mainbox" style={{ marginBottom: '20px' }}>
         {sprites.map(({ character, sprite }, index) => (
-          <div key={index} style={{ display: 'inline-block', marginRight: '10px' }}>
-            {sprite && <img src={sprite} alt={character} style={{ width: '150px', height: '150px' }} />}
-          
+          <div
+            id="profileImage"
+            key={index}
+            style={{ display: 'inline-block', marginRight: '10px' }}
+          >
+            {sprite && (
+              <img
+                src={sprite}
+                alt={character}
+                style={{ width: '150px', height: '150px' }}
+              />
+            )}
           </div>
         ))}
         <div>
-          <h2 id='spritebox'>{page.Character}</h2>
-          <p id='textbox'>{page.PageText}</p>
+          <h2 id="namebox">{page.Character}</h2>
+          <p id="textbox">{page.PageText}</p>
+          {page.Options ? (
+            <div>
+              {Object.keys(page.Options).map((option, index) => (
+                <button
+                  key={index}
+                  onClick={() => handleOptionClick(page.Options[option])}
+                >
+                  {option}
+                </button>
+              ))}
+            </div>
+          ) : (
+            <button onClick={handleNextPage}>Next</button>
+          )}
         </div>
       </div>
-      {page.Options ? (
-        <div>
-          {Object.keys(page.Options).map((option, index) => (
-            <button key={index} onClick={() => handleOptionClick(page.Options[option])}>
-              {option}
-            </button>
-          ))}
-        </div>
-      ) : (
-        <button onClick={handleNextPage}>Next</button>
-      )}
     </div>
   );
 };
