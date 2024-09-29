f911881222509d1ebceee9936eb1be67c798d2ff
Box-Cat <pjh211pjh@gmail.com>
2024-09-29 22:27:57 +0900

    배경음악 설정

--- src/components/VisualNovelEngine .jsx
+++ src/components/VisualNovelEngine .jsx
@@ -13,10 +13,45 @@ import ButtonContainer from './ButtonContainer';
 const VisualNovelEngine = () => {
   const [currentPage, setCurrentPage] = useState(0);
   const [data, setData] = useState(null);
+  const [audio, setAudio] = useState(null);
+  const [currentBGM, setCurrentBGM] = useState(null);
+  const BGM_VOLUME = 0.3; 
 
   useEffect(() => {
     setData(jsonData);
   }, []);
+
+  useEffect(() => {
+    if (data) {
+      const page = data.Scene1.PAGES['Page' + currentPage];
+      const pageBGM = page.BGM;
+
+      // 현재 페이지의 BGM이 있고, 현재 BGM과 다를 경우
+      if (pageBGM && pageBGM !== currentBGM) {
+        if (audio) {
+          audio.pause();  
+        }
+
+        const newAudio = new Audio(pageBGM);  
+        newAudio.loop = true;
+        newAudio.volume = BGM_VOLUME; 
+        newAudio.play();
+        setAudio(newAudio);
+        setCurrentBGM(pageBGM); 
+      } 
+      // 페이지에 BGM이 없고, 현재 BGM이 있을 경우
+      else if (!pageBGM && currentBGM) {
+        return;
+      }
+    }
+
+    // 컴포넌트가 언마운트될 때 오디오 정지
+    return () => {
+      if (audio) {
+        audio.pause();
+      }
+    };
+  }, [data, currentPage]);
 
   if (!data) return <div>Loading...</div>;
 
@@ -27,17 +62,12 @@ const VisualNovelEngine = () => {
 
   const handleNextPage = () => {
     const nextPage = data.Scene1.PAGES['Page' + currentPage].NextPage;
-    if (nextPage !== undefined) return; //"NextPage": "END" 가 있으면 Next 버튼 비활성화
+    if (nextPage !== undefined) return;  // "NextPage"가 있으면 Next 버튼 비활성화
     setCurrentPage(currentPage + 1);
   };
 
   const handleOptionClick = (option) => {
     setCurrentPage(option);
-  };
-
-  const handleButtonAction = (action) => {
-    if (action === 'before') handleBeforePage();
-    else if (action === 'next') handleNextPage();
   };
 
   const page = data.Scene1.PAGES['Page' + currentPage];
@@ -72,10 +102,10 @@ const VisualNovelEngine = () => {
         <NameBox name={page.Character} />
         <TextBox text={page.PageText} />
         <ButtonContainer>
-          <Button onKeyDown={handleButtonAction} onClick={handleBeforePage}>
+          <Button onClick={handleBeforePage}>
             Before
           </Button>
-          <Button onKeyDown={handleButtonAction} onClick={handleNextPage}>
+          <Button onClick={handleNextPage}>
             Next
           </Button>
         </ButtonContainer>
